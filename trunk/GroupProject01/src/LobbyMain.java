c26fb2d2ee7b525ef903d323491556de451ba234
riovalto <riovalto@62af3688-c0f5-9362-deb2-4f4f67cd4482>
2011-09-09 05:04:17 +0000

    - LobbyLogin : JDialog로 변경 (로그인 전에는 메인메뉴에 접근 못하도록) - LobbyMain : 채팅창 추가 수정 - UserInfo : getter(), setter() 추가

--- trunk/GroupProject01/src/LobbyMain.java
+++ trunk/GroupProject01/src/LobbyMain.java
@@ -8,8 +8,8 @@ import javax.swing.border.EtchedBorder;
 import javax.swing.table.DefaultTableModel;
 
 //로비화면의 실제 폼과 기능 구현
-public class LobbyMain extends JFrame implements ActionListener{
-	
+public class LobbyMain extends JFrame implements ActionListener {
+
 	// 보더효과와 카드레이아웃 설정
 	Border bdMainEdge = BorderFactory.createEtchedBorder(EtchedBorder.RAISED);
 	CardLayout card = new CardLayout();
@@ -18,8 +18,8 @@ public class LobbyMain extends JFrame implements ActionListener{
 	JMenuBar jmb = new JMenuBar();
 	JMenu jmGameMenu = new JMenu("게임");
 	JMenuItem jmLogin = new JMenuItem("로그인");
-	JMenuItem jmExit = new JMenuItem("종료");	
-	
+	JMenuItem jmExit = new JMenuItem("종료");
+
 	JPanel jpMain = new JPanel();
 	JPanel jpRightTab = new JPanel();
 	JPanel jpUserInfo = new JPanel();
@@ -29,14 +29,14 @@ public class LobbyMain extends JFrame implements ActionListener{
 	JPanel jpGameMain = new JPanel();
 
 	JPanel jpChat = new JPanel();
-	JTextPane jtpChatList = new JTextPane();
+	JTextArea jtaChatList = new JTextArea();
 	JTextField jtfChatInput = new JTextField();
 
-	//JFrame시작 위치를 잡기위한 툴킷과 좌표 설정
+	// JFrame시작 위치를 잡기위한 툴킷과 좌표 설정
 	Toolkit tkMyTool = Toolkit.getDefaultToolkit();
 	int positionX = tkMyTool.getScreenSize().width / 2 - 405;
 	int positionY = tkMyTool.getScreenSize().height / 2 - 325;
-	
+
 	public LobbyMain() {
 		super("Mini Game Paradise");
 
@@ -45,60 +45,61 @@ public class LobbyMain extends JFrame implements ActionListener{
 		jmGameMenu.add(jmExit);
 		jmb.add(jmGameMenu);
 		setJMenuBar(jmb);
-		
+
 		// 접속 유저 정보 테이블 생성
 		String col[] = { "아이디", "닉네임" };
-//		String row[][] = new String[0][2];
-		String row[][] = {{"test", "밥줘"}};
-		dtModel = new DefaultTableModel(row, col){
+		// String row[][] = new String[0][2];
+		String row[][] = { { "test", "밥줘" } };
+		dtModel = new DefaultTableModel(row, col) {
 			@Override
 			public boolean isCellEditable(int row, int column) {
 				// TODO Auto-generated method stub
 				return false;
 			}
 		};
-		jtUserList = new JTable(dtModel);		
+		jtUserList = new JTable(dtModel);
 
 		// Test//
-//		jpRightTab.setBackground(Color.RED);
-//		jpChat.setBackground(Color.GREEN);
+		jpRightTab.setBackground(Color.RED);
+		// jpChat.setBackground(Color.GREEN);
 		jpGameMain.setBackground(Color.BLUE);
-		
+		jtaChatList.setLineWrap(true);
 		// Test//
 
-		//채팅창 설정
-		jtpChatList.setEditable(false);		
+		// 채팅창 설정
+		jtaChatList.setEditable(false);
 		jpChat.setLayout(new GridBagLayout());
-		insert(jpChat, new JScrollPane(jtpChatList), 0, 0, 1, 1, 0.5, 0.9);
+		insert(jpChat, new JScrollPane(jtaChatList), 0, 0, 1, 1, 0.5, 0.9);
 		insert(jpChat, jtfChatInput, 0, 1, 1, 1, 0.5, 0.1);
-		
-		//유저 정보창 배치
+
+		// 유저 정보창 배치
 		jpRightTab.setLayout(new GridBagLayout());
 		insert(jpRightTab, jpUserInfo, 0, 0, 1, 1, 0.3, 0.3);
 		insert(jpRightTab, new JScrollPane(jtUserList), 0, 1, 1, 1, 0.7, 0.7);
-		
-		//패널 배치 (메인화면, 우측 정보창, 채팅창)
-		jpMain.setLayout(null);		
-		jpGameMain.setLayout(card);		
-		jpGameMain.setBounds(5, 5, 600, 430);		
-		jpMain.add(jpGameMain);	
+
+		// 패널 배치 (메인화면, 우측 정보창, 채팅창)
+		jpMain.setLayout(null);
+		jpGameMain.setLayout(card);
+		jpGameMain.setBounds(5, 5, 600, 430);
+		jpMain.add(jpGameMain);
 		jpChat.setBounds(5, 440, 600, 150);
 		jpMain.add(jpChat);
 		jpRightTab.setBounds(610, 5, 190, 585);
 		jpMain.add(jpRightTab);
 		jpMain.setBorder(bdMainEdge);
-		add(jpMain);		
-		
+		add(jpMain);
+
 		// 프레임 기본설정 (위치는 항상 중앙, 리사이징 불가)
 		setBounds(positionX, positionY, 810, 650);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setResizable(false);
 		setVisible(true);
-		
-		//최초 실행시 로그인창 띄움
-		new LobbyLogin(positionX, positionY);
-		
-		//액션이벤트 설정
+
+		// 최초 실행시 로그인창 띄움
+		new LobbyLogin(this);
+
+		// 액션이벤트 설정
+		jtfChatInput.addActionListener(this);
 		jmLogin.addActionListener(this);
 		jmExit.addActionListener(this);
 	}
@@ -109,18 +110,23 @@ public class LobbyMain extends JFrame implements ActionListener{
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+		
 		Object ob = e.getSource();
-		if(ob==jmLogin){
-			new LobbyLogin(positionX, positionY);
-		}
-		if(ob==jmExit){
+		
+		if (ob == jmLogin) {
+			new LobbyLogin(this);
+		} else if (ob == jmExit) {
 			System.exit(0);
 		}
+		
+		if (ob == jtfChatInput) {
+			jtaChatList.append(jtfChatInput.getText() + "\n");
+			jtfChatInput.setText("");
+		}
 	}
-	
-	private void insert(Container cnt, Component cmp, int x, int y, int w, int h, double wx,
-			double wy) {
+
+	private void insert(Container cnt, Component cmp, int x, int y, int w,
+			int h, double wx, double wy) {
 		GridBagConstraints gbc = new GridBagConstraints();
 		gbc.fill = GridBagConstraints.BOTH;
 		gbc.gridx = x;
--- trunk/GroupProject01/src/UserInfo.java
+++ trunk/GroupProject01/src/UserInfo.java
@@ -7,4 +7,78 @@ public class UserInfo {
 	private String userMemo;
 	private String userIp;
 	private String userPort;	
+	
+	public UserInfo(){
+		
+	}
+
+	public UserInfo(String userID, String userPW, String userName,
+			String userNickname, String userMemo, String userIp, String userPort) {
+		super();
+		this.userID = userID;
+		this.userPW = userPW;
+		this.userName = userName;
+		this.userNickname = userNickname;
+		this.userMemo = userMemo;
+		this.userIp = userIp;
+		this.userPort = userPort;
+	}
+
+	public String getUserID() {
+		return userID;
+	}
+
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+
+	public String getUserPW() {
+		return userPW;
+	}
+
+	public void setUserPW(String userPW) {
+		this.userPW = userPW;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserNickname() {
+		return userNickname;
+	}
+
+	public void setUserNickname(String userNickname) {
+		this.userNickname = userNickname;
+	}
+
+	public String getUserMemo() {
+		return userMemo;
+	}
+
+	public void setUserMemo(String userMemo) {
+		this.userMemo = userMemo;
+	}
+
+	public String getUserIp() {
+		return userIp;
+	}
+
+	public void setUserIp(String userIp) {
+		this.userIp = userIp;
+	}
+
+	public String getUserPort() {
+		return userPort;
+	}
+
+	public void setUserPort(String userPort) {
+		this.userPort = userPort;
+	}
+	
+
 }

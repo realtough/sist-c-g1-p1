d9c3ee2069f14a81048af2c99405fd7f68c549a4
sleekwook <sleekwook@62af3688-c0f5-9362-deb2-4f4f67cd4482>
2011-09-27 01:57:19 +0000

    정답까지

--- trunk/GroupProject01/src/com/sist/client/CatchMind.java
+++ trunk/GroupProject01/src/com/sist/client/CatchMind.java
@@ -1,34 +1,49 @@
 package com.sist.client;
+import javax.security.auth.callback.LanguageCallback;
 import javax.swing.*;
+
+import com.sist.common.G1Server;
 
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.image.BufferedImage;
-public class CatchMind extends JFrame  {
+public class CatchMind extends JFrame implements ActionListener {
 
 	PaintBoard pb = new PaintBoard();
 	
-	JTextField answer,write,id,sex;
-	JButton delete,alldelete,outbt;
+	JTextField answer2,write,id,sex;
+	JButton alldelete,outbt;
+	JLabel answer;
 	JTextPane chatting;
 	JPanel board;//그림판
+	String realanswer=new String();
+	
 	 
 	
 	
 	public CatchMind(){
-		answer=new JTextField();  //정답 텍스트 필드
+		answer2=new JTextField(10);
+		answer=new JLabel("정답", JLabel.LEFT);  //정답 텍스트 필드
 		write=new JTextField(30); //정답 쓰는 텍스트필드
 		id=new JTextField(15); //아이디 
 		sex=new JTextField(15); //성별
 			
-		delete=new JButton("지우개"); //지우개 버튼
+		
 		alldelete=new JButton("전체지움");//전체지움 버튼
 		outbt=new JButton("나가기");  //나가기 버튼	
 		chatting=new JTextPane(); //채팅이나오는 패널
 		board=new JPanel(); //그림판
 		
+	
+		
 		JPanel p1=new JPanel();
-		p1.add(delete);
+//		p1.setLayout(null);
+//		answer.setBounds(0, 20, 50, 20);
+//		alldelete.setBounds(70, 20, 50, 20);
+//		write.setBounds(130, 20, 50, 20);
+		
+		p1.add(answer);
+		p1.add(answer2);
 		p1.add(alldelete);
 		p1.add(write);
 		
@@ -58,9 +73,12 @@ public class CatchMind extends JFrame  {
 		setVisible(true);	
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
+		alldelete.addActionListener(this);
+		answer2.addActionListener(this);
+		outbt.addActionListener(this);
+		
 			
-		
-		
+			
 			
 	}
 	public static void main(String[] args) {
@@ -76,16 +94,17 @@ public class CatchMind extends JFrame  {
 		BufferedImage image = new BufferedImage(625, 530,2);	
 		Graphics2D imageG = image.createGraphics(); 
 		
-		
-		public PaintBoard(){
-			imageG.setColor(Color.yellow);//글씨색
-			imageG.drawString("왼쪽버튼을 누른 채로 마우스를 움직여보세요.",10,60);
-			imageG.fillRect(0, 0, 625, 530);
-			
-			
-		
+		public void painting(){
+			imageG.setColor(Color.yellow);//글씨색		
+			imageG.fillRect(0, 0, 625, 530);		
 			repaint();
 			imageG.setColor(Color.blue);
+		}
+		
+		
+		public PaintBoard(){
+			imageG.drawString("왼쪽버튼을 누른 채로 마우스를 움직여보세요.",10,60);
+			painting();
 			addMouseMotionListener(this);
 			
 		}	
@@ -94,8 +113,7 @@ public class CatchMind extends JFrame  {
 			   super.paintComponent(g);   
 			   Graphics2D tempg = (Graphics2D)g;			   
 			   tempg.drawImage(image, 0, 0, this);
-			  
-			   
+			  			   
 			  }
 		 
 		/*public void paintComponent (Graphics g){     //패인트
@@ -126,4 +144,25 @@ public class CatchMind extends JFrame  {
 	}
 	
 }
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource()==alldelete){
+			pb.painting();	
+		}
+		else if(e.getSource()==answer2){
+			answer2.setEditable(false);
+			realanswer=answer2.getText().trim();
+			if(realanswer.length()==0){
+				JOptionPane.showMessageDialog(this,"정답을 입력하세요");
+				answer2.setEditable(true);
+				
+			}else
+				answer2.setEditable(false);
+			answer2.setFocusable(false);			
+		}
+		if(e.getSource()==outbt){    //원래 패널 넘겨받음
+			
+		}
+	}
 }
